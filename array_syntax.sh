cf41fd20b5728c8c399acaf0bf4ae0c3a531466b
schoud100 <99477921+schoud100@users.noreply.github.com>
2022-02-13 13:02:49 +0530

    Create array_syntax.sh

---
+++ array_syntax.sh
@@ -0,0 +1,17 @@
+NAME[0]="Zara"
+NAME[1]="Qadir"
+NAME[2]="Mahnaz"
+NAME[3]="Ayan"
+NAME[4]="Daisy"
+echo "First Index: ${NAME[0]}"
+echo "Second Index: ${NAME[1]}"
+echo "First Method: ${NAME[*]}"
+echo "Second Method: ${NAME[@]}"
+
+
+
+#$./test.sh
+#First Index: Zara
+#Second Index: Qadir
+#First Method: Zara Qadir Mahnaz Ayan Daisy
+#Second Method: Zara Qadir Mahnaz Ayan Daisy
